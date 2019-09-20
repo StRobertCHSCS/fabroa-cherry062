b1605f58c6e12eeaf97ae5494d459803062a4a28
cherry062 <yiguo.yu21@ycdsbk12.ca>
2019-09-20 09:07:12 -0400

    aiuhf

---
+++ Working/1_1.py
@@ -0,0 +1,13 @@
+print("hello world!")
+print("This is the text of a print statement")
+print("What's wrong with this statement?")
+print("How about this one?")
+print(5)
+print(3.14)
+print("Here are the ages of my three children: ",12   ,7   ,4)
+print(3.14,"is my favourite pi")
+print("Hello world!")
+print(16)
+print("hi there!")
+
+
---
+++ Working/1_2.py
@@ -0,0 +1,8 @@
+print(12345//10)
+print(12345//100)
+print(12345//1000)
+
+print(12345 % 10)
+print("Delightful"*1000)
+print("|:.:*1000")
+print(2 + 3 * 4 + 8)
