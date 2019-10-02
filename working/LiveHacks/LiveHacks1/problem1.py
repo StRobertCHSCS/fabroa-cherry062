65374598265680155882edee16844b5c0927b410
cherry062 <yiguo.yu21@ycdsbk12.ca>
2019-10-02 09:35:15 -0400

    submit

---
+++ working/LiveHacks/LiveHacks1/problem1.py
@@ -0,0 +1,3 @@
+C=float(input('please enter a fahrenheit number'))
+F=9/5*C+32
+print(F)
---
+++ working/LiveHacks/LiveHacks1/problem2.py
@@ -0,0 +1,6 @@
+chicken=int(input('enter number of chicken'))
+student=int(input('enter number of student'))
+studentChicken=int(chicken/student)
+remainChicken=chicken%student
+print("each stuent will get", studentChicken,"pieces of chicken")
+print("Mr.Fabora will get",remainChicken,'pieces of chicked')
