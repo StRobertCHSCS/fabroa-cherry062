c575d2fbb3ce66cfaa03bc0df43824ef9d29d2ef
cherry062 <yiguo.yu21@ycdsbk12.ca>
2019-11-18 09:40:28 -0500

    livehack_problem2

---
+++ Working/LiveHackPractice/2_livehack_practice_solution2.py
@@ -0,0 +1,4 @@
+side1=(int(input("enter the lenghth of side 1")
+side2=(int(input("enter the lenghth of side 2")
+side3=(int(input("enter the lenghth of side 3"
+)
---
+++ working/LiveHacks/LiveHack2/problem#2-livehack2_problem2.py
@@ -0,0 +1,15 @@
+print("welcome to the triangle checker")
+angle1=int(input("enter the first angle"))
+angle2=int(input("enter the second angle"))
+angle3=int(input("enter the third angle"))
+if(angle1==60)and (angle2==60)and(angle3==60)
+  print("a triangle with angles"angle1,","angle2," "and",angle3,"forms as an equilateral triangle")
+  elif angle1+angle2+angle3==180
+      if (angle1==angle2)or(angle1==angle3)or(angle2==angle3):
+          print("a trianle with angles"angle1,","angle2,"and",angle3,"forms as an lsosceles triangle")
+   elif angle1+angle2+angle3==180
+   if (angle1!==angle2)or(angle1!==angle3)or(angle2!==angle3):
+print("a trianle with angles"angle1,","angle2,"and",angle3,"forms as an scalene triangle")
+else:
+    print("error,the angles do not from a triangle")
+    
